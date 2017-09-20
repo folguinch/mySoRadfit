f166e38705e46fb60937bda5b5595ff8d0f69eee
Fernando Olguin <f.olguin.ch@gmail.com>
2017-09-20 15:07:16 +0800

    Fix minor error

--- objects/register.py
+++ objects/register.py
@@ -4,5 +4,5 @@ http://scottlobdell.me/2015/08/using-decorators-python-automatic-registration/
 REGISTERED_CLASSES = {}
 
 def register_class(cls):
-    REGISTERED_CLASSES[lower(cls.__name__)] = cls
+    REGISTERED_CLASSES[cls.__name__.lower()] = cls
     return cls
