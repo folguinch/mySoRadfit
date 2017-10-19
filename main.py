f88217037cfcc81f8ca8e80bd799a60ef6a3b357
Fernando Olguin <f.olguin.ch@gmail.com>
2017-10-19 17:17:45 +0800

    Check data loading done for SED

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import os, argparse
 from configparser import ConfigParser
 
+from myutils.logger import get_logger
+
 from objects.source import LoadSource
+from utils.parsers.args import LoadConfig
 
 def main():
     """Main function for the RT fitter"""
-    # Look for configuration file
+    # Important standard files and directories
+    DIRECTORIES = 'directories.cfg'
     LOOKUP = ['~/.config/rt_fitter', './', './config']
+
+    # Logger
+    logger = get_logger(__name__)
+
+    # Look for configuration file
+    logger.debug('Loading main configuration file')
     for path in LOOKUP:
-        file_name = os.path.join(path,'directories.cfg')
+        file_name = os.path.join(path, DIRECTORIES)
         realpath = os.path.expanduser(path)
         if os.path.isdir(realpath) and os.path.isfile(file_name):
             config = ConfigParser()
@@ -16,13 +26,17 @@ def main():
             break
 
     # Read command line arguments
+    logger.debug('Loading line arguments')
     parser = argparse.ArgumentParser()
-    parser.add_argument('source', action=LoadSource, config=config)
+    #config = parser.add_argument('--config', action=LoadConfig, default=config)
     parser.add_argument('--stats', action='store_true', 
             help='Compute stats only')
     parser.add_argument('--use', nargs='*', type=list, default=['all'], 
             help='Use these data')
-    par = parser.parse_args()
+    parser.add_argument('source', action=LoadSource, 
+            source_dir=config.get('DIRECTORIES','sources'),
+            help='Source name')
+    args = parser.parse_args()
 
     # Workflow
 
