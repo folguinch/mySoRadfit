b6bba562f9296d92e165601b2306d90c9efa90aa
Fernando Olguin <f.olguin.ch@gmail.com>
2017-10-20 18:50:40 +0800

    Load hyperion model output and sed
    Loading images needs to be implemented

---
+++ rt/hyperion.py
@@ -0,0 +1,49 @@
+import os
+
+import astropy.units as u
+from hyperion.model import ModelOutput
+
+from ..objects.container import Container
+from ..register import REGISTERED_CLASSES
+
+class Hyperion(Container):
+    """Creates a container for Hyperion output data.
+
+    Attributes:
+        name: name of the model.
+        config: output configuration of the model.
+        data: data in the model.
+    """
+
+    def load_data(self, key, file_name=None, source=None, incl=None, angle=None,
+            dtype=None):
+        """Load data for key.
+
+        Parameters:
+            key: data to load.
+            filename: file to open.
+            source: source object.
+            incl: inclination index.
+            angle: inclination angle (model config must be pre-loaded)
+            dtype: data type.
+        """
+        assert incl is not None or (angle is not None and \
+                self.config is not None)
+
+        if key=='model':
+            assert os.path.isfile(file_name)
+            self.data[key] = ModelOutput(file_name)
+        elif file_name and dtype:
+            assert os.path.isfile(file_name)
+            self.data[key] = load_data_by_type(file_name, dtype.lower(), 
+                    REGISTERED_CLASSES)
+        elif key=='sed':
+            wlg, F = self.data['model'].get_sed(group=0,
+                    distance=source.distance.cgs.value, inclination=incl,
+                    units='Jy')
+            data = np.array(zip(wlg, F), dtype=[('wlg', float),('F', float)])
+            self.data[key] = SED(data=data, 
+                    units={'wlg':1.*u.micron, 'F':1.*u.Jy})
+        else:
+            raise NotImplementedError
+
