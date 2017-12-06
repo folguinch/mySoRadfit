3cd38c5bdff1e4888c0c644b2468691d34a2f184
Fernando Olguin <f.olguin.ch@gmail.com>
2017-12-06 19:12:06 +0800

    Include flared disc

---
+++ distributions/discs.py
@@ -0,0 +1,34 @@
+import numpy as np
+from hyperion.util.integrate import integrate_powerlaw
+
+def flared(r, th, params):
+
+    # Reference density
+    rmin = params.getfloat('Disc','rmin') *\
+            params.getquantity('Disc','rsub').cgs
+    rmax = params.getquantity('Disc','rmax').cgs
+    beta = params.getfloat('Disc','beta')
+    p = beta - params.getfloat('Disc','alpha')
+    r0 = params.getquantity('Disc','r0').cgs
+    h0 = params.getquantity('Disc','h0').cgs
+    intg = integrate_powerlaw(rmin, rmax, 1.0 + p)
+    intg = int1 * r0**-p
+    intg = (2.*np.pi)**1.5 * h0 * intg
+    rho0 = params.getquantity('Disc','m').cgs/intg
+
+    # Convert coordinates
+    R = r.cgs * np.sin(th.to(u.rad))
+    z = r.cgs * np.cos(th.to(u.rad))
+
+    # Scale height
+    h = h0 * (R/r0)**beta
+
+    # Density
+    density = rho0 * (r0/R)**(beta-p) * np.exp(-0.5*(z/h)**2)
+    density[r.cgs<rmin] = 0.
+    density[r.cgs>rmax] = 0.
+
+    return density
+
+
+
