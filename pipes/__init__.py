577234073f02289133b35eaf8533180d2e34f061
Fernando Olguin <f.olguin.ch@gmail.com>
2018-08-23 18:36:02 +0800

    Track pipes init

---
+++ pipes/__init__.py
@@ -0,0 +1,12 @@
+from os.path import basename, dirname, join
+from glob import glob
+
+from myutils.decorators import REGISTERED_FUNCTIONS, REGISTERED_CLASSES
+
+# Load all the classes
+pwd = dirname(__file__)
+for x in glob(join(pwd, '*.py')):
+    if not x.startswith('__'):
+        __import__(basename(x)[:-3], globals(), locals())
+
+__all__ = ['REGISTERED_FUNCTIONS', 'REGISTERED_CLASSES']
