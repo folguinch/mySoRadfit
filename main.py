6d036be56df0714158ec1e88564df40b7c25e11d
Fernando Olguin <f.olguin.ch@gmail.com>
2018-09-07 17:36:58 +0800

    Implement Bayes fitting

--- main.py
+++ main.py
@@ -1,50 +1,59 @@
 import os, argparse
 from configparser import ConfigParser
 
-from myutils.logger import get_logger
+#from myutils.logger import get_logger
+import astropy.units as u
+from myutils.argparse_actions import startLogger, LoadConfig
+from astroSource.source import LoadSourcefromConfig
 
-from MySoRadfit.objects.source import LoadSource
-from MySoRadfit.rt.hyperion_model import Hyperion
+#from mySoRadfit import ModelfromConfig
+from mySoRadfit.pipes.bayes_fitting import bayes_pipe
 #from utils.parsers.args import LoadConfig
 
-def main():
-    """Main function for the RT fitter"""
-    # Important standard files and directories
-    DIRECTORIES = 'directories.cfg'
-    LOOKUP = ['~/.config/MySoRadfit', './MySoRadfit', './MySoRadfit/config']
+#def criterion(cell):
+#    if cell.mass >= 1*u.M_sun:
+#        return True
+#    else:
+#        return False
 
-    # Logger
-    logger = get_logger(__name__)
-
-    # Look for configuration file
-    logger.debug('Loading main configuration file')
-    for path in LOOKUP:
-        file_name = os.path.join(path, DIRECTORIES)
-        realpath = os.path.expanduser(path)
-        if os.path.isdir(realpath) and os.path.isfile(file_name):
-            config = ConfigParser()
-            config.read(file_name)
-            break
-
-    # Read command line arguments
-    logger.debug('Loading line arguments')
+def get_parser():
     parser = argparse.ArgumentParser()
     #config = parser.add_argument('--config', action=LoadConfig, default=config)
     parser.add_argument('--stats', action='store_true', 
             help='Compute stats only')
+    parser.add_argument('--bayes', action='store_true', 
+            help='Use MCMC+bayes model fitting')
+    parser.add_argument('--logger', action=startLogger, 
+            help='Main logger file name')
     parser.add_argument('--use', nargs='*', type=list, default=['all'], 
             help='Use these data')
-    parser.add_argument('source', action=LoadSource, 
-            source_dir=config.get('DIRECTORIES','sources'),
-            help='Source name')
+    parser.add_argument('--nwalkers', nargs=1, type=int, default=[2],
+            help='Number of walkers for MCMC as a function of parameters')
+    parser.add_argument('--steps', nargs=1, type=int, default=[100],
+            help='Number of MCMC steps')
+    parser.add_argument('--source', action=LoadSourcefromConfig,
+            help='Source configuration file')
+    parser.add_argument('--outconfig', action=LoadConfig,
+            help='Postprocessing configuration')
+    #parser.add_argument('model', action=ModelfromConfig, 
+    #        help='Model configuration file')
+    parser.add_argument('model', type=str, 
+            help='Model configuration file')
+    return parser
+
+def main():
+    """Main function for the RT fitter"""
+
+    # Read command line arguments
+    parser = get_parser()
     args = parser.parse_args()
 
     # Workflow
-    model = Hyperion('Model.0009')
-    model.load_data('model',
-            os.path.expanduser('~/Sources/AFGL2591/Models/hyperion/Model.0009.rtout'))
-    model.load_data('sed', source=args.source, incl=0)
-    print model['sed'].interpolate(args.source['sed']['wlg'])
+    if args.bayes:
+        args.logger.info('Running Bayesian modelling')
+        bayes_pipe(args)
+    else:
+        raise NotImplementedError
     
 if __name__=='__main__':
     main()
