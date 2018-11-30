d79bbae4bc1040996e19ae67b3f997ec52471d14
Fernando Olguin <f.olguin.ch@gmail.com>
2018-11-30 17:06:38 +0800

    Implement CASA pipe

--- pipes/line_pipes.py
+++ pipes/line_pipes.py
@@ -4,6 +4,8 @@ import astropy.units as u
 from myutils.decorators import register_function
 from myutils.logger import get_logger
 from myutils.montage import rotate
+#from myutils.script_list import casa_sim
+from myutils.external import run_bash
 
 from ..utils.mollie import load_model_cube
 
@@ -32,12 +34,24 @@ def casa_base_pipe(config, model_file, source, PA):
     filename = mollie_base_pipe(model_file, source, PA=PA)
 
     # Run CASA
-
-    return concatname
+    simulated = model_file + '_simulated.fits'
+    casa_sim = os.path.dirname(os.path.realpath(__file__))
+    casa_sim = os.path.normpath(os.path.join(casa_sim,
+        '../run_casa_simulator.sh'))
+    assert os.path.isfile(casa_sim)
+    cmd = "%s %s %s %s" % (casa_sim, config['casa_sim'], filename, simulated)
+    #run_casa(casa_sim, config['casa_sim'], filename, simulated)
+    run_bash(cmd)
+    print "Simulation finished"
+    #os.system('rm -rf CONF*')
+    assert os.path.isfile(simulated)
+    
+    return simulated
 
 @register_function
 def mollie_casa_line_pv(config, model_file, source, PA=0.*u.deg,
         vlsr=0.*u.km/u.s, logger=get_logger(__name__)):
+    logger.info('Running Mollie-CASA line pv pipeline')
     # Run casa
     filename = casa_base_pipe(config, model_file, source, PA)
     print filename
@@ -47,4 +61,14 @@ def mollie_casa_line_pv(config, model_file, source, PA=0.*u.deg,
 
     return data
 
+@register_function
+def mollie_casa_line_cube(config, model_file, source, PA=0.*u.deg,
+        vlsr=0.*u.km/u.s, logger=get_logger(__name__)):
+    logger.info('Running Mollie-CASA line cube pipeline')
+    # Run casa
+    filename = casa_base_pipe(config, model_file, source, PA)
 
+    # Load cube
+    cube = None
+
+    return cube
